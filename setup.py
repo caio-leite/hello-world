65c43473cd22d9207981d225f9e0d4c6e2338ae2
Caio Henrique Ortelan Leite <caioleite@grupoboticario.com.br>
2021-10-04 12:08:07 -0300

    add:

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="caioleite",
-    version="1.0.9",
+    version="1.1.0",
     author="caioleite",
     author_email="caioleite@grupoboticario.com.br",
     description="It's pip... with git.",
